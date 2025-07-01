c05ead13038a0ed3a87925bee9d54f18d1438b2a
satvinder <satvinder1504@gmail.com>
2025-07-01 18:21:12 +0530

    page is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Cloud from './Components/Service/Cloud/Cloud';
+import CareerPage from './Components/Career/CareerPage';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         {/* Sample route */}
       <Route path="/Cloud" element={<Cloud />} />
+      <Route path="/CareerPage" element={<CareerPage />} />
       </Routes>
     </Router>
   );
